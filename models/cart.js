e3fd5279252f1ed8feb5af66de067fbada69b9d0
ryonakamura-st-mac <nakamura.ryo@seedtech.co.jp>
2023-08-13 16:03:11 +0800

    refactored

--- models/cart.js
+++ models/cart.js
@@ -15,20 +15,15 @@ module.exports = class Cart {
       }
       // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex((p) => p.id === id);
-      const existingProduct = cart.products[existingProductIndex];
-      let updatedProduct;
-      if (existingProduct) {
-        updatedProduct = { ...existingProduct };
-        updatedProduct.qty = updatedProduct.qty + 1;
-        cart.products = [...cart.products];
-        cart.products[existingProductIndex] = updatedProduct;
-      } else {
-        updatedProduct = { id: id, qty: 1 };
-        cart.products = [...cart.products, updatedProduct];
-      }
-      cart.totalPrice = cart.totalPrice + productPrice;
+
+      existingProductIndex !== -1
+        ? cart.products[existingProductIndex].qty++
+        : cart.products.push({ id, qty: q });
+
+      cart.totalPrice += productPrice;
+
       fs.writeFile(p, JSON.stringify(cart), (err) => {
-        console.log(err);
+        if (err) console.log(err);
       });
     });
   }
